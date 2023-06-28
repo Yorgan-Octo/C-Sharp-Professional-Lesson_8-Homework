a6998feb44c9e9336dbaf6e08ef52e5dd2921cb1
Artem Pr <yorgan.info@gmail.com>
2023-06-28 16:33:01 +0300

    ЗАвершено завдання 5

---
+++ Task_3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task_3
+{
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string name, string fullName, int age)
+        {
+            Name = name;
+            FullName = fullName;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name};");
+            Console.WriteLine($"Фамилия: {FullName};");
+            Console.WriteLine($"Вік: {Age};");
+        }
+
+    }
+
+    [XmlRoot("MyUser")]
+    public class UserCastom
+    {
+
+        [XmlAttribute("UName")]
+        public string Name { get; set; }
+
+        [XmlAttribute("UFullName")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("UAge")]
+        public int Age { get; set; }
+
+        public UserCastom()
+        {
+        }
+
+        public UserCastom(string name, string fullName, int age)
+        {
+            Name = name;
+            FullName = fullName;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name};");
+            Console.WriteLine($"Фамилия: {FullName};");
+            Console.WriteLine($"Вік: {Age};");
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user1;
+            UserCastom user2;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(User));
+            FileStream sile = File.Open("file-bas.xml", FileMode.Open);
+
+
+            user1 = serializer.Deserialize(sile) as User;
+            user1.PrintInfo();
+
+            Console.WriteLine(new String('-', 50));
+
+
+
+            XmlSerializer serializer2 = new XmlSerializer(typeof(UserCastom));
+            FileStream sile2 = File.Open("file-Atr.xml", FileMode.Open);
+
+
+            user2 = serializer2.Deserialize(sile2) as UserCastom;
+            user2.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_5/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Task_5
+{
+
+    public enum TypeLight
+    {
+        Daylight,
+        Bicolor,
+        Rgb
+    }
+
+
+    [Serializable]
+    public class LoghtMonobloc
+    {
+        [JsonProperty("Name Light")]
+        public string Name { get; set; }
+
+        [JsonProperty("Light RCA")]
+        public int Rca { get; set; }
+
+        [JsonProperty("Type Light")]
+        public TypeLight Light { get; set; }
+
+        [JsonProperty("Min Photometrics")]
+        public int MinPhotometrics { get; set; }
+
+        [JsonProperty("Max Photometrics")]
+        public int MaxPhotometrics { get; set; }
+
+        public LoghtMonobloc(string name, int rca, TypeLight ling, int minPhotometrics, int maxPhotometrics)
+        {
+            Name = name;
+            Rca = rca;
+            Light = ling;
+            MinPhotometrics = minPhotometrics;
+            MaxPhotometrics = maxPhotometrics;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name Light: {Name};");
+            Console.WriteLine($"Light RCA: {Rca};");
+            Console.WriteLine($"Type Light: {Light};");
+            Console.WriteLine($"Min Photometrics: {MinPhotometrics};");
+            Console.WriteLine($"Max Photometrics: {MaxPhotometrics};");
+        }
+
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LoghtMonobloc loght = new LoghtMonobloc("GF500", 93, TypeLight.Daylight, 2000, 10000);
+            string file = JsonConvert.SerializeObject(loght, Formatting.Indented, new StringEnumConverter());
+
+            Console.WriteLine(file);
+            Console.ReadKey();
+
+            LoghtMonobloc loght2;
+            loght2 = JsonConvert.DeserializeObject<LoghtMonobloc>(file);
+            Console.WriteLine(new String('-', 50));
+            loght2.PrintInfo();
+            Console.ReadKey();
+
+
+            //или так:
+            //FileStream stream = File.Create("CarData.data");
+            //BinaryFormatter formatter = new BinaryFormatter();
+
+            //formatter.Serialize(stream, loght);
+            //stream.Close();
+
+            //FileStream fs = File.Open("CarData.data", FileMode.Open);
+            //LoghtMonobloc car = formatter.Deserialize(fs) as LoghtMonobloc;
+            //fs.Close();
+
+            //Console.ReadLine();
+
+
+        }
+    }
+}
